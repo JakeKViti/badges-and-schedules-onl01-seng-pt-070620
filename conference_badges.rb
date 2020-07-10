0e377ba09f6cd93afb5adc4ca3039ecc4d830e7a
Jacob Viti <thevitij64@gmail.com>
2020-07-10 02:52:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def badge_maker(attendees)
+  return "Hello, my name is #{attendees}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.each  {|name| puts  "Hello, my name is #{attendees}."}
+end
+
+def assign_rooms(attendees)
+
+end
+
+def printer(attendees)
+
+end
