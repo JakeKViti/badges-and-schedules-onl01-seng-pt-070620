1dd791ceb1c9f67a1be5a71a66160b70ad7c8011
Jacob Viti <thevitij64@gmail.com>
2020-07-10 04:17:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ end
 def assign_rooms(attendees)
   rooms = []
   counter = 1
-  attendees.each do {|name| rooms.push "Hello, #{name}! You'll be assigned to room #{counter}!"}
+  attendees.each do {|name| rooms.push `Hello, #{name}! You'll be assigned to room #{counter}!`}
     counter += 1
   end
   return rooms
